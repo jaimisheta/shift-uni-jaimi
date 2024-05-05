21c93498945a5cb54739d12262987feac5b12f67
Jaimi Maheshbhai Sheta <jm504814@dal.ca>
2024-05-05 10:45:34 -0400

    Favourite items stored in local storage

--- src/store/dataListSlice.js
+++ src/store/dataListSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   dataList: [],
-  favouritemList: [],
+  favouritemList: JSON.parse(localStorage.getItem("favouritemList")) || [],
   apiDetails: {},
 };
 
@@ -14,26 +14,23 @@ export const dataSlice = createSlice({
       state.dataList = action.payload.data;
       state.apiDetails = action.payload.apiDetails;
     },
-    updatefavouritemList: (state, action) => {
-      state.favouritemList = action.payload;
-    },
     addTofavouritemList: (state, action) => {
-      state.favouritemList = [...state.favouritemList, action.payload];
+      const favouritemList = [...state.favouritemList, action.payload];
+      localStorage.setItem("favouritemList", JSON.stringify(favouritemList));
+      state.favouritemList = favouritemList;
     },
     removeFromfavouritemList: (state, action) => {
-      state.favouritemList = state.favouritemList.filter(
+      const favouritemList = state.favouritemList.filter(
         (item) => item.name !== action.payload.name
       );
+      localStorage.setItem("favouritemList", JSON.stringify(favouritemList));
+      state.favouritemList = favouritemList;
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const {
-  updateDataList,
-  updatefavouritemList,
-  addTofavouritemList,
-  removeFromfavouritemList,
-} = dataSlice.actions;
+export const { updateDataList, addTofavouritemList, removeFromfavouritemList } =
+  dataSlice.actions;
 
 export default dataSlice.reducer;
